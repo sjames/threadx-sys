ac823d90154d0a1978f2685e73c0dc823977c971
Sojan James <sojan.james@gmail.com>
2023-12-24 18:02:49 +0530

    Added two more functions that are implemented in assembly for the CortexM

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,6 @@ include!("generated.rs");
 // Functions that are implemented in assembly that are missed by bindgen
 extern "C" {
     pub fn _tx_timer_interrupt() -> ();
+    pub fn __tx_SVCallHandler() -> ();
+    pub fn __tx_PendSVHandler() -> ();
 }
